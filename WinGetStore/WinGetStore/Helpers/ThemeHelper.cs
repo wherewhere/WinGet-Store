512c470a26b5be88464364814757b13b3322246c
wherewhere <qq2518766683@outlook.com>
2023-08-24 17:21:16 +0800

    ThemeHelper 加入异步方法
    更新 WinGet OOP/WinRT 清单

--- WinGetStore/WinGetStore/Helpers/ThemeHelper.cs
+++ WinGetStore/WinGetStore/Helpers/ThemeHelper.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Toolkit.Uwp.Helpers;
+using System.Linq;
+using System.Threading.Tasks;
 using Windows.ApplicationModel.Core;
 using Windows.UI;
 using Windows.UI.Core;
@@ -20,82 +22,160 @@ namespace WinGetStore.Helpers
 
         public static WeakEvent<bool> UISettingChanged { get; } = new WeakEvent<bool>();
 
+        #region ActualTheme
+
         /// <summary>
         /// Gets the current actual theme of the app based on the requested theme of the
         /// root element, or if that value is Default, the requested theme of the Application.
         /// </summary>
-        public static ElementTheme ActualTheme
-        {
-            get
-            {
-                return CurrentApplicationWindow == null
-                    ? SettingsHelper.Get<ElementTheme>(SettingsHelper.SelectedAppTheme)
-                    : CurrentApplicationWindow.Dispatcher.HasThreadAccess
-                        ? CurrentApplicationWindow.Content is FrameworkElement rootElement
-                            && rootElement.RequestedTheme != ElementTheme.Default
-                                ? rootElement.RequestedTheme
-                                : SettingsHelper.Get<ElementTheme>(SettingsHelper.SelectedAppTheme)
-                        : UIHelper.AwaitByTaskCompleteSource(() =>
-                            CurrentApplicationWindow.Dispatcher.AwaitableRunAsync(() =>
-                                CurrentApplicationWindow.Content is FrameworkElement _rootElement
-                                    && _rootElement.RequestedTheme != ElementTheme.Default
-                                        ? _rootElement.RequestedTheme
-                                        : SettingsHelper.Get<ElementTheme>(SettingsHelper.SelectedAppTheme),
-                                CoreDispatcherPriority.High));
-            }
-        }
+        public static ElementTheme ActualTheme => GetActualTheme();
+
+        public static ElementTheme GetActualTheme() =>
+            CurrentApplicationWindow == null
+                ? SettingsHelper.Get<ElementTheme>(SettingsHelper.SelectedAppTheme)
+                : CurrentApplicationWindow.Dispatcher.HasThreadAccess
+                    ? CurrentApplicationWindow.Content is FrameworkElement rootElement
+                        && rootElement.RequestedTheme != ElementTheme.Default
+                            ? rootElement.RequestedTheme
+                            : SettingsHelper.Get<ElementTheme>(SettingsHelper.SelectedAppTheme)
+                    : UIHelper.AwaitByTaskCompleteSource(() =>
+                        CurrentApplicationWindow.Dispatcher.AwaitableRunAsync(() =>
+                            CurrentApplicationWindow.Content is FrameworkElement _rootElement
+                                && _rootElement.RequestedTheme != ElementTheme.Default
+                                    ? _rootElement.RequestedTheme
+                                    : SettingsHelper.Get<ElementTheme>(SettingsHelper.SelectedAppTheme),
+                            CoreDispatcherPriority.High));
+
+        public static async Task<ElementTheme> GetActualThemeAsync() =>
+            CurrentApplicationWindow == null
+                ? SettingsHelper.Get<ElementTheme>(SettingsHelper.SelectedAppTheme)
+                : CurrentApplicationWindow.Dispatcher.HasThreadAccess
+                    ? CurrentApplicationWindow.Content is FrameworkElement rootElement
+                        && rootElement.RequestedTheme != ElementTheme.Default
+                            ? rootElement.RequestedTheme
+                            : SettingsHelper.Get<ElementTheme>(SettingsHelper.SelectedAppTheme)
+                    : await CurrentApplicationWindow.Dispatcher.AwaitableRunAsync(() =>
+                        CurrentApplicationWindow.Content is FrameworkElement _rootElement
+                            && _rootElement.RequestedTheme != ElementTheme.Default
+                                ? _rootElement.RequestedTheme
+                                : SettingsHelper.Get<ElementTheme>(SettingsHelper.SelectedAppTheme),
+                            CoreDispatcherPriority.High);
+
+        #endregion
+
+        #region RootTheme
 
         /// <summary>
         /// Gets or sets (with LocalSettings persistence) the RequestedTheme of the root element.
         /// </summary>
         public static ElementTheme RootTheme
         {
-            get
-            {
-                return CurrentApplicationWindow == null
-                    ? ElementTheme.Default
-                    : CurrentApplicationWindow.Dispatcher.HasThreadAccess
-                        ? CurrentApplicationWindow.Content is FrameworkElement rootElement
-                            ? rootElement.RequestedTheme
-                            : ElementTheme.Default
-                        : UIHelper.AwaitByTaskCompleteSource(() =>
-                            CurrentApplicationWindow.Dispatcher.AwaitableRunAsync(() =>
-                                CurrentApplicationWindow.Content is FrameworkElement _rootElement
-                                    ? _rootElement.RequestedTheme
-                                    : ElementTheme.Default,
-                                CoreDispatcherPriority.High));
-            }
-            set
-            {
-                if (CurrentApplicationWindow == null) { return; }
-                value.UpdateWindowRequestedTheme();
-                value.UpdateSystemCaptionButtonColors();
-                SettingsHelper.Set(SettingsHelper.SelectedAppTheme, value);
-                UISettingChanged.Invoke(value.IsDarkTheme());
-            }
+            get => GetRootTheme();
+            set => SetRootTheme(value);
         }
+
+        public static ElementTheme GetRootTheme() =>
+            CurrentApplicationWindow == null
+                ? ElementTheme.Default
+                : CurrentApplicationWindow.Dispatcher.HasThreadAccess
+                    ? CurrentApplicationWindow.Content is FrameworkElement rootElement
+                        ? rootElement.RequestedTheme
+                        : ElementTheme.Default
+                    : UIHelper.AwaitByTaskCompleteSource(() =>
+                        CurrentApplicationWindow.Dispatcher.AwaitableRunAsync(() =>
+                            CurrentApplicationWindow.Content is FrameworkElement _rootElement
+                                ? _rootElement.RequestedTheme
+                                : ElementTheme.Default,
+                            CoreDispatcherPriority.High));
+
+        public static async Task<ElementTheme> GetRootThemeAsync() =>
+            CurrentApplicationWindow == null
+                ? ElementTheme.Default
+                : CurrentApplicationWindow.Dispatcher.HasThreadAccess
+                    ? CurrentApplicationWindow.Content is FrameworkElement rootElement
+                        ? rootElement.RequestedTheme
+                        : ElementTheme.Default
+                    : await CurrentApplicationWindow.Dispatcher.AwaitableRunAsync(() =>
+                        CurrentApplicationWindow.Content is FrameworkElement _rootElement
+                            ? _rootElement.RequestedTheme
+                            : ElementTheme.Default,
+                        CoreDispatcherPriority.High);
+
+        public static async void SetRootTheme(ElementTheme value)
+        {
+            WindowHelper.ActiveWindows.ForEach(async (window) =>
+            {
+                await window.Dispatcher.ResumeForegroundAsync();
+                if (window.Content is FrameworkElement rootElement)
+                {
+                    rootElement.RequestedTheme = value;
+                }
+            });
+
+            SettingsHelper.Set(SettingsHelper.SelectedAppTheme, value);
+            UpdateSystemCaptionButtonColors();
+            UISettingChanged.Invoke(await IsDarkThemeAsync());
+        }
+
+        public static async Task SetRootThemeAsync(ElementTheme value)
+        {
+            await Task.WhenAll(WindowHelper.ActiveWindows.Select(async (window) =>
+            {
+                await window.Dispatcher.ResumeForegroundAsync();
+                if (window.Content is FrameworkElement rootElement)
+                {
+                    rootElement.RequestedTheme = value;
+                }
+            }));
+
+            SettingsHelper.Set(SettingsHelper.SelectedAppTheme, value);
+            UpdateSystemCaptionButtonColors();
+            UISettingChanged.Invoke(await IsDarkThemeAsync());
+        }
+
+        #endregion
 
         static ThemeHelper()
         {
+            // Save reference as this might be null when the user is in another app
+            CurrentApplicationWindow = Window.Current;
+            RootTheme = SettingsHelper.Get<ElementTheme>(SettingsHelper.SelectedAppTheme);
+
             // Registering to color changes, thus we notice when user changes theme system wide
             UISettings = new UISettings();
             UISettings.ColorValuesChanged += UISettings_ColorValuesChanged;
         }
 
-        public static void Initialize(Window window)
+        public static async void Initialize(Window window)
         {
             CurrentApplicationWindow ??= window;
+            if (window?.Content is FrameworkElement rootElement)
+            {
+                rootElement.RequestedTheme = await GetActualThemeAsync();
+            }
             UpdateSystemCaptionButtonColors(window);
         }
 
-        private static void UISettings_ColorValuesChanged(UISettings sender, object args)
+        private static async void UISettings_ColorValuesChanged(UISettings sender, object args)
         {
             UpdateSystemCaptionButtonColors();
-            UISettingChanged.Invoke(IsDarkTheme());
+            UISettingChanged.Invoke(await IsDarkThemeAsync());
         }
 
         public static bool IsDarkTheme()
         {
+            return Window.Current != null
+                ? ActualTheme == ElementTheme.Default
+                    ? Application.Current.RequestedTheme == ApplicationTheme.Dark
+                    : ActualTheme == ElementTheme.Dark
+                : ActualTheme == ElementTheme.Default
+                    ? UISettings?.GetColorValue(UIColorType.Background) == Colors.Black
+                    : ActualTheme == ElementTheme.Dark;
+        }
+
+        public static async Task<bool> IsDarkThemeAsync()
+        {
+            ElementTheme ActualTheme = await GetActualThemeAsync();
             return Window.Current != null
                 ? ActualTheme == ElementTheme.Default
                     ? Application.Current.RequestedTheme == ApplicationTheme.Dark
@@ -116,18 +196,6 @@ namespace WinGetStore.Helpers
                     : ActualTheme == ElementTheme.Dark;
         }
 
-        public static void UpdateWindowRequestedTheme(this ElementTheme theme)
-        {
-            WindowHelper.ActiveWindows.ForEach(async (window) =>
-            {
-                await window.Dispatcher.ResumeForegroundAsync();
-                if (window.Content is FrameworkElement rootElement)
-                {
-                    rootElement.RequestedTheme = theme;
-                }
-            });
-        }
-
         public static void UpdateSystemCaptionButtonColors(this ElementTheme theme)
         {
             bool isDark = theme.IsDarkTheme();
@@ -137,7 +205,7 @@ namespace WinGetStore.Helpers
         public static async void UpdateSystemCaptionButtonColors()
         {
             await CurrentApplicationWindow.Dispatcher.ResumeForegroundAsync();
-            bool isDark = IsDarkTheme();
+            bool isDark = await IsDarkThemeAsync();
             UpdateSystemCaptionButtonColors(isDark);
         }
 
@@ -147,7 +215,7 @@ namespace WinGetStore.Helpers
         public static async void UpdateSystemCaptionButtonColors(this Window window)
         {
             await CurrentApplicationWindow.Dispatcher.ResumeForegroundAsync();
-            bool isDark = IsDarkTheme();
+            bool isDark = await IsDarkThemeAsync();
             window.UpdateSystemCaptionButtonColors(isDark);
         }
 
