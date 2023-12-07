026839fb864c4c179ae56b86bc336e99b3b396b8
wherewhere <qq2518766683@outlook.com>
2023-12-07 23:12:20 +0800

    磁贴更新间隔滑条提示加入格式化

---
+++ WinGetStore/WinGetStore/Helpers/Converters/TimeSpanToolTipValueConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace WinGetStore.Helpers.Converters
+{
+    public class TimeSpanToolTipValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            TimeSpan time = value switch
+            {
+                TimeSpan timeSpan => timeSpan,
+                string @string => TimeSpan.Parse(@string),
+                _ => TimeSpan.FromMinutes(System.Convert.ToDouble(value)),
+            };
+            return ConverterTools.Convert(time.ToString(), targetType);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (targetType == typeof(string))
+            {
+                return value.ToString();
+            }
+            else
+            {
+                TimeSpan timeSpan = TimeSpan.Parse(value.ToString());
+                if (targetType == typeof(TimeSpan))
+                {
+                    return timeSpan;
+                }
+                else
+                {
+                    return ConverterTools.Convert(timeSpan.TotalMinutes, targetType);
+                }
+            }
+        }
+    }
+}
